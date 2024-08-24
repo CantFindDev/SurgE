5b579e6af39698eb8dc987b2de843c73fa0a42ef
CantFind <62027437+CantFindDev@users.noreply.github.com>
2024-08-24 23:13:25 +0300

    Liscense updates

--- Bot.py
+++ Bot.py
@@ -4,6 +4,21 @@ from dotenv.main import load_dotenv
 from discord.ext import commands
 import discord
 
+ # SurgE Growtopia surgery simulator discord bot
+ # Copyright (C) 2024 CantFind
+ #
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU Affero General Public License as published
+ # by the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ #  This program is distributed in the hope that it will be useful,
+ #  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ #  GNU Affero General Public License for more details.
+ #
+ # You should have received a copy of the GNU Affero General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
